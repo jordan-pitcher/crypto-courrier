575633df4c9dedd2a4207706b67e648b58aac66d
Jordan <35932248+jordan-pitcher@users.noreply.github.com>
2018-05-14 23:41:16 -0700

    Update cryptobot.py

--- CryptoCourrier/cryptobot.py
+++ CryptoCourrier/cryptobot.py
@@ -2,12 +2,26 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 from discord import Client
+from coinmarketcap import Market
 import os
 
 Client = discord.Client()
 cl = commands.Bot(command_prefix="$")
 cl.run(os.environ['BOT_TOKEN'])
+cmc = Market()
 
+'''
+display the top crypto currency stats from coinmarketcap
+'''
+
+@cl.command(pass_context=True)
+@commands.cooldown(1, 60, commands.BucketType.user) 
+async def top(ctx, cnt):
+     
+    coin_stats = coinmarketcap.ticker(limit=cnt)
+    await cl.say(f"Displaying top {cnt} crypto currencies from CoinMarketCap")
+
+    
 
 @cl.event
 async def on_ready():
