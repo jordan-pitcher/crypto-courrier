858ac3a6c32a45d882eaa503075da6ac9b644877
jordan-pitcher <35932248+jordan-pitcher@users.noreply.github.com>
2018-05-14 20:23:34 -0700

    Update cryptobot.py

--- CryptoCourrier/cryptobot.py
+++ CryptoCourrier/cryptobot.py
@@ -1,1 +1,24 @@
-print('1')
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+from discord import Client
+import os
+
+Client = discord.Client()
+cl = commands.Bot(command_prefix="$")
+cl.run(os.environ['BOT_TOKEN'])
+
+
+@cl.event
+async def on_ready():
+    
+  online_message = 'deploying to cloud...'
+  print (online_message)
+    
+      
+@cl.event
+async def on_message(message):
+
+  msg = message.content
+  print(msg)
+  
